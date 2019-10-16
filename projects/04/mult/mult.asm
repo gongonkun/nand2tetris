2d9211e657fb00dad8e3eb03840903b6284b47b6
gongonkun <922d.sakata@gmail.com>
2019-10-16 22:24:23 +0900

    add mult.asm

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,49 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// r2 = 0
+@2
+M=0
+
+// if (r0 == 0) return
+@0
+D=M
+@END
+D;JEQ
+
+// if (r1 == 0) return
+@1
+D=M
+@END
+D;JEQ
+
+// r3 = r1
+@1
+D=M
+@3
+M=D
+
+// while(true) {
+(LOOP)
+  // r2 += r0
+  @0
+  D=M
+  @2
+  M=M+D
+
+  // r3 -= 1
+  @3
+  M=M-1
+  @3
+  D=M
+
+  // if (r3 <= 0) break
+  @END
+  D;JLE
+
+// }
+  @LOOP
+  0;JMP
+(END)
+@END
+0;JMP
