29a4e71f11d70fa6b73a9d6a6990a7e34fd2e65c
gongonkun <922d.sakata@gmail.com>
2019-10-22 20:10:42 +0900

    add Fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -10,5 +10,50 @@
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
+@8192
+D=A
+@max_size
+M=D
+(INPUT)
+  @crr_count
+  M=0
+  @KBD
+  D=M
+  @WHITE
+  D;JEQ
+  @BLACK
+  D;JMP
 
-// Put your code here.
+(BLACK)  
+  @crr_count
+  D=M
+  M=M+1
+  @SCREEN
+  A=A+D
+  M=-1
+
+  @crr_count
+  D=M
+  @max_size
+  D=D-M
+  @BLACK
+  D;JNE
+  @INPUT
+  0;JMP
+
+(WHITE)
+  @crr_count
+  D=M
+  M=M+1
+  @SCREEN
+  A=A+D
+  M=0
+
+  @crr_count
+  D=M
+  @max_size
+  D=D-M
+  @WIHTE
+  D;JEQ
+  @INPUT
+  0;JMP
